86abd27f2a6995c8f144afc4695ef6afa7a80d1f
Colbee Thrasher <colbeethr@gmail.com>
2025-09-14 23:16:42 -0500

    asked user for fav school sub and gave responce

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,25 +6,66 @@ namespace SelectionStatementExercise
     {
         static void Main(string[] args)
         {
+           
+            //first exercise
             int favNumber = 11;
             Console.WriteLine("guess my favorite number");
             int userInput = int.Parse(Console.ReadLine());
-
-
+            
+            
             if (userInput < favNumber)
             {
                 Console.WriteLine("too low");
             }
-
+            
             else if (userInput > favNumber)
             {
                 Console.WriteLine("too high");
+                
             }
             else
             {
                 Console.WriteLine("you guessed it!");
             }
-    }
+            //second exercise
+
+            Console.WriteLine("What is your favorite school subject");
+            string schoolSubject = Console.ReadLine();
+            
+            switch (schoolSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is such a cool subject i love solving problems");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is a cool subject because you get to learn about so many cool things'");
+                    break;
+                case "History":
+                    Console.WriteLine("I love how we keep th history of our world");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a great subject to expand your knowledge base");
+                    break;
+                case "Chemistry":
+                    Console.WriteLine("i like Chemistry because you get to do experiments");
+                    break;
+                default:
+                    Console.WriteLine("that's a great choice What do you like about it");
+                    break;
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
         
     }
 }
