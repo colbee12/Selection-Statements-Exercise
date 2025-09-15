509a4f1cc019fcd1c56c38fa33bd422e538f5a37
Colbee Thrasher <colbeethr@gmail.com>
2025-09-14 22:25:49 -0500

    made a program that has user guess my fav number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            int favNumber = 11;
+            Console.WriteLine("guess my favorite number");
+            int userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("you guessed it!");
+            }
+    }
+        
     }
 }
